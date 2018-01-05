4f6f1a6884c5e0f95f627eb1decf656341be9cee
Gnana Allan Whinney G <gnana.gnanapragasam@viasat.com>
2018-01-05 19:21:36 +0530

    (find-trip)-1 working copy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,41 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ FormsModule, ReactiveFormsModule ] 
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+it('form valid when sumbitted ', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const familynameInput = fixture.debugElement.query(By.css('#familyName'));
+  const codeInput = fixture.debugElement.query(By.css('#code'));
+  let familyname = familynameInput.nativeElement;
+  let code = codeInput.nativeElement;
+  familyname.value = "Allan";
+  code.value = "789GH";
+  const familynameValue = fixture.debugElement.query(By.css('#familynameMandate'));
+  const codeValue = fixture.debugElement.query(By.css('#codeMandate'));
+  expect(familynameValue).toBe(null);
+  expect(codeValue).toBe(null);
+});
+
+it('form invalid when form empty and sumbitted ', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.debugElement.query(By.css('#retriveBooking')).nativeElement.click();
+  fixture.detectChanges();
+  const familynameValue = fixture.debugElement.query(By.css('#familynameMandate')).nativeElement.innerText;
+  const codeValue = fixture.debugElement.query(By.css('#codeMandate')).nativeElement.innerText;
+  expect(familynameValue).toEqual("please enter a valid family name.");
+  expect(codeValue).toEqual("please enter a valid booking code.");
+});
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  tripFinder : 	FormGroup;
+
+constructor(private fb: FormBuilder) {}
+
+  ngOnInit(){
+  	this.tripFinder = this.fb.group({
+  		 code: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(6),Validators.pattern('^$|^[A-Za-z2-9]+')]],
+  		 familyName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30),Validators.pattern('^$|^[A-Za-z]+')]]
+  		});
+  	}
+
+  onSubmit(form: FormGroup) {
+
+  }
 }
